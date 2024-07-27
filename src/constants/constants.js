79782816709461b84a5f0a0d761579ef03854aca
Vanshaj Pahwa <vanshajpahwa07@gmail.com>
2024-07-27 14:42:59 +0530

    feat: Add a movie server with minimal ads

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 export const embededMovieUrls = {
-    server1: "https://vidsrc.to/embed/movie/",
-    server2: "https://vidsrc.xyz/embed/",
-    server3: "https://multiembed.mov/directstream.php?video_id="
+    server1: "https://vidsrc.pro/embed/movie/",
+    server2: "https://vidsrc.to/embed/movie/",
+    server3: "https://vidsrc.xyz/embed/",
+    server4: "https://multiembed.mov/directstream.php?video_id="
 }
 export const embededSeriesUrls = {
     server1: "https://vidsrc.xyz/embed/tv",
